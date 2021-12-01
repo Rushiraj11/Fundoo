d2c37a341110cfabdfc5a6e79c8b3abbb04ed1dc
Rushiraj11 <grushiraj95@gmail.com>
2021-12-01 14:26:44 +0530

    Modified:Removed sample component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-// import Registeration from './pages/signup/Registeration';
-// import Testing from './pages/Testing/Testing';
-import Smaple from './components/Smaple';
+import Registeration from './pages/signup/Registeration';
+
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Registeration /> */}
-      {/* <Testing /> */}
-      <Smaple />
+      <Registeration />
+     
+    
     </div>
   );
 }
