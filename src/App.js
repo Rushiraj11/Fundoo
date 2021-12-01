5c69aad27aaf10a8151d3dedca3b7d6af1b87a52
Rushiraj11 <grushiraj95@gmail.com>
2021-12-01 16:40:22 +0530

    Modified: App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-<<<<<<< HEAD
-import './App.css';
-import Registeration from './pages/signup/Registeration';
-
-
-=======
 
 import './App.css';
 import Registeration from './pages/signup/Registeration';
 
->>>>>>> Signin
 
 function App() {
   return (
     <div className="App">
       <Registeration />
-<<<<<<< HEAD
-     
-    
-=======
->>>>>>> Signin
     </div>
   );
 }
