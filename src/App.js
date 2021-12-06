ec284e930e66143d7c6298dc0b73490926031609
Rushiraj11 <grushiraj95@gmail.com>
2021-12-06 10:46:54 +0530

    Added:Dashboard

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 
 import './App.css';
+import Dashboard from './pages/Dashboard/Dashboard';
 //import Registeration from './pages/signup/Registeration';
 //  import Login from './pages/Login/Login';
-import Forget from './pages/Forget/Forget';
+// import Forget from './pages/Forget/Forget';
 // import Reset from './pages/Reset/Reset';
+
 
 function App() {
   return (
     <div className="App">
       {/* <Registeration /> */}
       {/* <Login /> */}
-      <Forget />
+      {/* <Forget /> */}
       {/* <Reset /> */}
+      <Dashboard />
     </div>
   );
 }
